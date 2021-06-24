547d6fc9a8e2e8bbf7be56df0f5cb57a5a07052f
Xiangxu Liu <xxliu@toughtworks.com>
2021-06-24 12:35:21 +0800

    fix tilte

--- source/conf.py
+++ source/conf.py
@@ -46,6 +46,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.env/*']
 # a list of builtin themes.
 #
 html_theme = 'furo'
+html_title = "XOps Fun"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
