c3141be5d0029f5b3019ab47ee2651d381db5ece
Xiangxu Liu <xxliu@toughtworks.com>
2021-06-24 12:14:18 +0800

    install furo

--- docs/build.sh
+++ docs/build.sh
@@ -9,7 +9,7 @@ export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
 
 # Install #
 python -m pip install -U Sphinx==4.0.2
-python -m pip install -U sphinx-book-theme
+python -m pip install -U furo
 
 
 # BUILD DOCS #
